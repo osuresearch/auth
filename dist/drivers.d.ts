01beb5c909f8f2a76a4014df01b479eb9621ab8e
Chase McManning <mcmanning.1@osu.edu>
2020-09-24 15:39:32 -0400

    Experimenting with different build paths

---
+++ dist/drivers.d.ts
@@ -0,0 +1,8 @@
+import GraphQL from './drivers/GraphQL';
+import JsonApi from './drivers/JsonApi';
+declare const _default: {
+    GraphQL: typeof GraphQL;
+    JsonApi: typeof JsonApi;
+};
+export default _default;
+//# sourceMappingURL=drivers.d.ts.map
---
+++ src/drivers.ts
@@ -0,0 +1,8 @@
+
+import GraphQL from './drivers/GraphQL';
+import JsonApi from './drivers/JsonApi';
+
+export default {
+    GraphQL,
+    JsonApi
+};
