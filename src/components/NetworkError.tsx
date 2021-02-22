5298eb8073b03fd1356212bcbff9cca56f054984
Chase McManning <mcmanning.1@osu.edu>
2021-02-22 18:43:33 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/components/NetworkError.tsx
+++ src/components/NetworkError.tsx
@@ -9,7 +9,7 @@ const NetworkError: React.FC<{ error?: string }> = ({ error }) => {
     return (
         <FullPageMessage title="Network Error">
             <p className="lead">
-                We are unable to communicate with the server. Please refresh the page and try again.
+                We are unable to communicate with the server. Please make sure you are connected to the internet, then refresh the page and try again.
             </p>
 
             <ContactUs title="Network Error" error={error} />
