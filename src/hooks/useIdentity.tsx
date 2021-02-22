a67c01b3aeb7615fd10d1793f5a92d69f6c75bb8
Chase McManning <mcmanning.1@osu.edu>
2021-02-22 13:52:59 -0500

    Add a more developer friendly error for when useIdentity is used outside of a configured context

--- src/hooks/useIdentity.tsx
+++ src/hooks/useIdentity.tsx
@@ -7,7 +7,16 @@ import { ConnectionState, IHasPolicies } from '../types';
  * IdM identity access
  */
 function useIdentity() {
-    return useContext(AuthContext);
+    const context = useContext(AuthContext);
+
+    if (typeof context.verifyLogin === 'undefined') {
+        throw new Error(
+            'Cannot call `useIdentity` outside an authenticated context. ' +
+            'Make sure it is only called from within an <AuthProvider>.'
+        );
+    }
+
+    return context;
 }
 
 export default useIdentity;
