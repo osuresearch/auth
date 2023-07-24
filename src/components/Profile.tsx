3103a8fa8c830b02f7a3e04575ce4895914a35dd
Chase McManning <cmcmanning@gmail.com>
2023-07-24 14:48:44 -0400

    fix(Profile): change profile link to a button
    closes #2

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,49 +1,54 @@
+import React from "react";
 
-import React from 'react';
-
-import Emulation from './Emulation';
-import useIdentity from '../hooks/useIdentity';
+import Emulation from "./Emulation";
+import useIdentity from "../hooks/useIdentity";
 
 export interface Props {
-    /**
-     * URL to redirect a user to edit their profile information.
-     * Defaults to `/register` on the same host.
-     */
-    editUrl?: string;
+  /**
+   * URL to redirect a user to edit their profile information.
+   * Defaults to `/register` on the same host.
+   */
+  editUrl?: string;
 }
 
-const Profile: React.FC<Props> = ({ editUrl = '/register', children }) => {
-    const { user, logout } = useIdentity();
+const Profile: React.FC<Props> = ({ editUrl = "/register", children }) => {
+  const { user, logout } = useIdentity();
 
-    // Not logged in - nothing to show.
-    if (!user) {
-        return null;
-    }
-    
-    return (
-        <div className="profile dropdown">
-            {/* TODO: This needs to be a button, not an href="#" element to appease the React accessibility gods.
-                But the CSS fix for that to be a button needs to be updated in ORIS/UI - not here. 
-            */}
-            <a href="#" className="dropdown-toggle"
-                id="profile-dropdown" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                {user.username}
-            </a>
-            <div className="dropdown-menu dropdown-menu-right" aria-labelledby="profile-dropdown">
-                <a className="dropdown-item" href={editUrl}>
-                    My Profile
-                </a>
+  // Not logged in - nothing to show.
+  if (!user) {
+    return null;
+  }
 
-                {children}
+  return (
+    <div className="profile dropdown">
+      <button
+        className="dropdown-toggle"
+        id="profile-dropdown"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        {user.username}
+      </button>
+      <div
+        className="dropdown-menu dropdown-menu-right"
+        aria-labelledby="profile-dropdown"
+      >
+        <a className="dropdown-item" href={editUrl}>
+          My Profile
+        </a>
 
-                <div className="dropdown-divider"></div>
+        {children}
 
-                <Emulation className="dropdown-item" />
-                <button className="dropdown-item profile-logout" onClick={logout}>Logout</button>
-            </div>
-        </div>
-    );
-}
+        <div className="dropdown-divider"></div>
+
+        <Emulation className="dropdown-item" />
+        <button className="dropdown-item profile-logout" onClick={logout}>
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
